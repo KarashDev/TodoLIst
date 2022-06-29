457af7a98d22494b4224757f48579024d0f410b9
Ivan Karash <karash.www@gmail.com>
2022-06-29 06:37:59 +0300

    Add project files.

---
+++ TodoLIst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoLIst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
